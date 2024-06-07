446132309e161b3660f8269e0fc7c3dffb6e2b56
0silo0 <126387303+0silo0@users.noreply.github.com>
2024-06-07 19:54:40 +0300

    Убрал в Program.cs лишние строки, которые были после pull перед мержем в эту ветку

--- ConsoleProject/Program.cs
+++ ConsoleProject/Program.cs
@@ -15,7 +15,6 @@ public class Program
 		var host = CreateHostBuilder(args).Build();
 		var bot = host.Services.GetRequiredService<Bot>();
 		bot.StartAsync().Wait();
-		var host = CreateHostBuilder(args).Build();
 
 		using (var scope = host.Services.CreateScope())
 		{
@@ -58,9 +57,6 @@ public class Program
 		Console.Clear();
         var bot = new Bot(token);
 		*/
-
-        var bot = new Bot("7382436094:AAHdjujRTLSXCQFzozdmJWQl-RiZOsXmcak");
-        bot.StartAsync().Wait();
 	}
 
 	private static IHostBuilder CreateHostBuilder(string[] args) =>
