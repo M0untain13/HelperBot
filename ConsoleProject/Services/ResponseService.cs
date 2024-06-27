123dc8e15d30cb108ea455d003cc29c09dfa94b3
M0untain13 <dan2002.dan2002@mail.ru>
2024-06-27 21:39:00 +0500

    Создал новый сервис ожидания ответов

--- ConsoleProject/Services/ResponseService.cs
+++ ConsoleProject/Services/ResponseService.cs
@@ -1,88 +1,181 @@
+﻿using ConsoleProject.Types;
 using Telegram.Bot;
 using Telegram.Bot.Types;
-using ConsoleProject.Types;
 
 namespace ConsoleProject.Services;
 
-public class ResponseService
+public enum SessionState
 {
-	private readonly Dictionary<long, (Mutex, Queue<Task>, Queue<MessageHandle>)> _waitingForResponse;
+	Wait,
+	Open,
+	Close
+}
 
-	public ResponseService()
+public class SessionProxy
+{
+	private Session _session;
+
+	public SessionState State => _session.State;
+
+	public SessionProxy(Session session)
 	{
-		_waitingForResponse = new Dictionary<long, (Mutex, Queue<Task>, Queue<MessageHandle>)>();
+		_session = session;
+	}
+	
+	public bool Add(Task task, MessageHandle handle) => _session.Add(task, handle);
+	public void Start() => _session.Start();
+	public void Close() => _session.Close();
+}
+
+public class Session
+{
+	private List<Task> _tasks;
+	private List<MessageHandle> _handles;
+	private SessionState _state;
+	public SessionState State => _state;
+
+	public Session()
+	{
+		_tasks = new List<Task>();
+		_handles = new List<MessageHandle>();
+		_state = SessionState.Wait;
 	}
 
-	public bool IsResponseExpected(long id)
+	public bool Add(Task task, MessageHandle handle)
 	{
-		return _waitingForResponse.ContainsKey(id) && _waitingForResponse[id].Item3.Count > 0;
-	}
-
-	/// <summary>
-	/// Обработать ответ.
-	/// </summary>
-	/// <returns> true - обработано, иначе false </returns>
-	public async Task<bool> ReplyAsync(ITelegramBotClient botClient, Message message)
-	{
-		var user = message.From;
-		if (user is null)
-			return false;
-
-		var id = user.Id;
-
-		if (IsResponseExpected(id))
+		if (_state != SessionState.Close)
 		{
-			await _waitingForResponse[id].Item3.Dequeue().Invoke(botClient, message);
-			if(_waitingForResponse[id].Item2.TryDequeue(out var action))
-			{
-				action.RunSynchronously();
-			}
-			else
-			{
-				_waitingForResponse[id].Item1.ReleaseMutex();
-			}
+			_tasks.Add(task);
+			_handles.Add(handle);
 			return true;
 		}
 
 		return false;
 	}
 
-	/// <summary>
-	/// Добавить действие в очередь.
-	/// После этого метода нужно вызвать StartActions.
-	/// </summary>
-	/// <param name="id"> Telegram ID. </param>
-	/// <param name="action"> Действие перед ожиданием ответа. </param>
-	/// <param name="handle"> Обработчик ответа. </param>
-	public async Task AddActionForWaitAsync(long id, Task action, MessageHandle handle)
+	private bool InvokeTask()
 	{
-		await Task.Run(() =>
-		{
-			if (!IsResponseExpected(id))
-			{
-				_waitingForResponse[id] = (new Mutex(), new Queue<Task>(), new Queue<MessageHandle>());
-			}
-
-			_waitingForResponse[id].Item1.WaitOne();
-			_waitingForResponse[id].Item2.Enqueue(action);
-			_waitingForResponse[id].Item3.Enqueue(handle);
-			_waitingForResponse[id].Item1.ReleaseMutex();
-		});
-	}
-
-	/// <summary>
-	/// Начать цепочку действий.
-	/// Использовать только после AddActionForWait.
-	/// </summary>
-	/// <param name="id"> Telegram ID. </param>
-	/// <returns> true - цепочка запущена, иначе false </returns>
-	public async Task<bool> StartActionsAsync(long id)
-	{
-		if (!IsResponseExpected(id))
+		if (_tasks.Count == 0 && _state == SessionState.Open)
 			return false;
 
-		_waitingForResponse[id].Item1.WaitOne();
-		_waitingForResponse[id].Item2.Dequeue().RunSynchronously();
+		var task = _tasks[0];
+		_tasks.Remove(task);
+		task.RunSynchronously();
+
 		return true;
+	}
+
+	public bool InvokeHandle(ITelegramBotClient botClient, Message message)
+	{
+		if (_handles.Count == 0 && _state == SessionState.Open)
+			return false;
+
+		var handle = _handles[0];
+		_handles.Remove(handle);
+		handle(botClient, message);
+
+		if (_tasks.Count > 0)
+			InvokeTask();
+
+		if (_handles.Count == 0)
+			Wait();
+
+		return true;
+	}
+
+	public void Start()
+	{
+		if(_state != SessionState.Close)
+		{
+			_state = SessionState.Open;
+			InvokeTask();
+		}
+	}
+
+	public void Wait()
+	{
+		if (_state != SessionState.Close)
+			_state = SessionState.Wait;
+	}
+	
+	public void Close()
+	{
+		_state = SessionState.Close;
+	}
+}
+
+public class ResponseService
+{
+	private Dictionary<long, List<Session>> _sessions;
+
+	public ResponseService()
+	{
+		_sessions = new Dictionary<long, List<Session>>();
+	}
+
+	public SessionProxy CreateSession(long id)
+	{
+		if (!_sessions.ContainsKey(id))
+			_sessions[id] = new List<Session>();
+
+		var session = new Session();
+		_sessions[id].Add(session);
+
+		return new SessionProxy(session);
+	}
+
+	public void Reply(ITelegramBotClient botClient, Message message)
+	{
+		var user = message.From;
+		if (user is null)
+			return;
+
+		var id = user.Id;
+
+		var session = GetSession(id);
+
+		if (session is null)
+			return;
+
+		// TODO: возможно тут стоит сделать выход из цикла, если слишком долгое ожидание
+		while(session.State == SessionState.Wait)
+			Thread.Sleep(1000);
+
+		session.InvokeHandle(botClient, message);
+	}
+
+	private Session? GetSession(long id)
+	{
+		if (!_sessions.ContainsKey(id))
+			return null;
+
+		// Я знаю, это ужасная конструкция.
+		// Но, как по мне, код читаем, а значит все норм!
+		while (true)
+		{
+			if (_sessions[id].Count == 0)
+				return null;
+
+			var session = _sessions[id][0];
+			switch (session.State)
+			{
+				case SessionState.Close:
+					_sessions[id].Remove(session);
+					break;
+				default:
+					return session;
+			}
+		}
+	}
+
+	
+	public SessionProxy? GetSessionProxy(long id)
+	{
+		var session = GetSession(id);
+
+		if (session is null)
+			return null;
+
+		return new SessionProxy(session);
 	}
 }
