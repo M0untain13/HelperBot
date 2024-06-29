3cca50e5f0a07864b4b6b95d2b73439712a8b81c
M0untain13 <dan2002.dan2002@mail.ru>
2024-06-29 20:04:23 +0500

    Исправил пример скрипта

--- ConsoleProject/start_example.sh
+++ ConsoleProject/start_example.sh
@@ -1,2 +1,2 @@
-!/bin/bash
-dotnet run <token> Host=<host>;Port=<port>;Database=<database>;Username=<user>;Password=<pass> 86400000 86400000 11111
+#!/bin/bash
+dotnet run <token> "Host=<host>;Port=<port>;Database=<database>;Username=<user>;Password=<pass>" 86400000 86400000 11111
